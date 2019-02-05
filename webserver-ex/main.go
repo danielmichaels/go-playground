3cbd4027eb028520159554b358933a12fd4d426f
Dan <dans.address@outlook.com>
2019-02-05 13:25:50 +1100

    webserver now increments user hits

--- webserver-ex/main.go
+++ webserver-ex/main.go
@@ -1,21 +1,36 @@
+/* program returns number of hits per page of webserver */
 package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
 )
+
+var counter int
+var mutex = &sync.Mutex{} // prevents race condition
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	http.HandleFunc("/", echoString)
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
+	http.HandleFunc("/increment", incrementCounter)
 
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hi!</h1>")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
