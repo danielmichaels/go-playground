2b4eae41dc9a0552e497364ce3199e3e74bed315
Dan <dans.address@outlook.com>
2019-02-05 14:04:46 +1100

    load json resp into structs and for loop over pokemon; done

--- pokeapi/main.go
+++ pokeapi/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,7 +9,27 @@ import (
 	"os"
 )
 
+// structs to store the pokeapi JSON response into
+
+// A response struct to map entire response
+type Response struct {
+	Name    string    `json:"name"`
+	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+// A pokemon struct that maps each pokemon
+type Pokemon struct {
+	EntryNo int            `json:"entry_number"`
+	Species PokemonSpecies `json:"pokemon_species"`
+}
+
+// A struct to map pokemon species to
+type PokemonSpecies struct {
+	Name string `json:"name"`
+}
+
 func main() {
+
 	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
 
 	if err != nil {
@@ -22,6 +43,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(responseData))
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject.Name)
+	fmt.Println(len(responseObject.Pokemon))
+
+	for i := 0; i < len(responseObject.Pokemon); i++ {
+		fmt.Println(responseObject.Pokemon[i].Species.Name)
+	}
+
+	// fmt.Println(string(responseData)) // prints entire GET response
 
 }
