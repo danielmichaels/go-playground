e6dc0eb1a5fcbf050244a960e1d9abe84c9cd792
Dan <dans.address@outlook.com>
2019-02-05 13:50:57 +1100

    JSON tutorial utilising Pokeapi GET requests done

---
+++ pokeapi/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(responseData))
+
+}
